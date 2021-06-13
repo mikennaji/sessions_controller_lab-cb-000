9a8246d655497ad1ba56d3f83a379eccfd9cc272
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-13 23:00:42 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,19 @@
 class SessionsController < ApplicationController
   before_action :require_login
   skip_before_action :require_login, only: [:new, :create]
-  
+
 def new
 
 end
 
 
 def create
-   session[:name] = params[:name]
-   require_login and return
-   redirect_to '/'
-
+  if params[:name].blank?
+           redirect_to login_path
+       else
+           session[:name] = params[:name]
+           redirect_to root_path
+       end
 end
 
 
